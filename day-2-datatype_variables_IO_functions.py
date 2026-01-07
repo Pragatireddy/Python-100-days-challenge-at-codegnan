ad647fc6616e8be8d8b3706daff140837cb1ba91
Pragatireddy <162780412+Pragatireddy@users.noreply.github.com>
2026-01-07 17:04:18 +0530

    Add files via upload

---
+++ day-2-datatype_variables_IO_functions.py
@@ -0,0 +1,20 @@
+#Datatype,variables and io functions
+name="Rani" #string type
+age=40 #integer type
+weight=89.12 #float datatype
+list1=[2,3,4,5,3,'Hi',(34,45)] # list of items
+tuple1=(10,12,23,'WELCOME','HELLO') #Tuple of items
+cards={'A',1,2,3,4,5,6,7,8,9,10,'J','K',2,3,5} #pack of cards in set data type
+student_marks={"maths":99,"telugu":89,"english":78}
+print("My name is:",name)
+print("My age is:",age)
+print("My weight is:",weight)
+print("list of numbers:",list1)
+print("tuple items:",tuple1)
+print("Pack of cards values:",cards)
+print("Student subject wise marks:",student_marks)
+
+
+
+
+
